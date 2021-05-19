edaf8c5cfad180e863838a971beb9f69b828aa3f
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2021-05-19 15:38:56 -0400

    working on avatar picture in relation with CLoudinary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,12 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+  end
 
   private
 
