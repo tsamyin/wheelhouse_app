6295ee900d03d0f9f4dce1daba5272621e462f1a
Thomas Sam-Yin-Yang <thomas.samyinyang@gmail.com>
2021-05-19 13:53:20 -0400

    Added the skip before action authenticate user for the index and show of tiny homes.

--- app/controllers/tiny_homes_controller.rb
+++ app/controllers/tiny_homes_controller.rb
@@ -1,4 +1,5 @@
 class TinyHomesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_tiny_home, only: [:show, :edit, :update, :destroy]
 
   def index
