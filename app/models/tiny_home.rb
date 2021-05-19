2a7642c528fb1e88f815e60e4a9f1062c0ba060a
Thomas Sam-Yin-Yang <thomas.samyinyang@gmail.com>
2021-05-19 12:22:29 -0400

    Added the pundit gem and user authorization.

--- app/models/tiny_home.rb
+++ app/models/tiny_home.rb
@@ -1,6 +1,6 @@
 class TinyHome < ApplicationRecord
   belongs_to :user
-  has_many :home_amenities
+  has_many :home_amenities, dependent: :destroy
   has_many :amenities, through: :home_amenities
   has_many :users
   has_many :bookings
