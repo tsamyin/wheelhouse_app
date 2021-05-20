9105ec2817a15b3f71bd12d13c3900a0fdba55d1
HollyHilts <holly.a.hilts@gmail.com>
2021-05-20 18:01:01 -0400

    created approved column for bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_194217) do
+ActiveRecord::Schema.define(version: 2021_05_20_214141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_194217) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "approved", default: false
     t.index ["tiny_home_id"], name: "index_bookings_on_tiny_home_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
